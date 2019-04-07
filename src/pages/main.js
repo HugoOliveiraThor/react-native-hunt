0a48a7bb5119d03397bf72dd16d69b75cf713098
hugo oliveira <hugooliveirahenrique@gmail.com>
2019-04-07 18:53:51 -0300

    add axios return

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 import { View , Text } from 'react-native';
 
 export default class Main extends Component {
 
     static navigationOptions = {
         title: 'Hunter'
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products')
+        const { docs } = response.data
+        console.log(docs)
     }
 
     render() {
