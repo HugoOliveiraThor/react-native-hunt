462a0f3de42f2df0f9dc0667f6e53f5287c7181d
hugo oliveira <hugooliveirahenrique@gmail.com>
2019-04-07 18:32:25 -0300

    Add navbar style

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View , Text } from 'react-native';
 
 export default class Main extends Component {
+
+    static navigationOptions = {
+        title: 'Hunter'
+    }
+
     render() {
         return (
             <View>
